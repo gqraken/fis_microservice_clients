2e5eb1717c8fa1fdec14595caa1d2bec0e794c51
gqraken <korreo@gmail.com>
2020-12-14 20:08:57 +0100

    Fixed path PUT method

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.post(BASE_API_PATH + "/clients", (req, res) => {
     });
 });
 
-app.put(BASE_API_PATH + "/clients/:clientName", (req, res) => {
+app.put(BASE_API_PATH + "/clients", (req, res) => {
     console.log(Date() + " - PUT /clients");
     var contact = req.body;
     db.update(req.body._id, contact, (err)=>{
