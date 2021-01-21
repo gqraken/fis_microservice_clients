f12fe5c023e7d5ae21cb34456b014b2c433870b2
gqraken <61391908+gqraken@users.noreply.github.com>
2021-01-21 20:04:51 +0100

    Added authentication

---
+++ auth.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const TOKEN = (process.env.TOKEN_SECRET || "fdgdfgdfgdfglkñjewlklekjwrljlkjsadlfkniu023984093840293lkhjkldf");
+
+module.exports =  async function (req, res, next){
+    const token = req.header('auth-token');
+    if(!token) return res.status(401).send('Access denied');
+
+    try{
+        // Verify token
+        const verified = jwt.verify(token,TOKEN);
+        
+        req.user = verified;
+        next();
+
+    } catch(err){
+        res.status(400).send({ msg: 'Error checking token: ', err: err});
+    }
+}
