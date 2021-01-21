69890f390ee9a86fc8393f091e73aed01090c848
gqraken <61391908+gqraken@users.noreply.github.com>
2021-01-21 20:05:47 +0100

    Added new client fields

--- clients.js
+++ clients.js
@@ -1,18 +1,22 @@
 const mongoose = require('mongoose');
 
 const clientSchema = new mongoose.Schema({
-    nif: String,
-    name: String,
-    address: String,
-    mail: String,
-    phone: Number
-});
+    username: { type: String, required: true, unique: true, trim: true, minlength: 3, maxlength: 32 },
+    password: { type: String, required: true, minlength: 6, maxlengt: 512 },
+    firstName: { type: String, required: true, trim: true, maxlength: 128 },
+    lastName: { type: String, required: true, trim: true, maxlength: 128 },
+    address: { type: String, required: true, trim: true, maxlength: 255 },
+    email: { type: String, required: true, unique: true, trim: true, minlength: 6, maxlength: 255 },
+    phone: { type: String, required: true, unique: true, trim: true },
+}, { 
+    timestamps: true,
+}
+);
 
 clientSchema.methods.cleanup = function(){
-    return {nif: this.nif, name: this.name,address: this.address,mail: this.mail, phone: this.phone};
+    return {username: this.username, password: this.password, firstName: this.firstName, lastName: this.lastName, address: this.address,email: this.email, phone: this.phone};
 }
 
 const Client = mongoose.model('Client', clientSchema);
 
 module.exports = Client;
-
