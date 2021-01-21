e69415a811902dbee688d5932cda89c39edf24f9
gqraken <61391908+gqraken@users.noreply.github.com>
2021-01-21 20:06:32 +0100

    added new methods to register and login

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 const Client = require('./clients');
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const authValidation = require('./auth');
+const {loginValidation, registerValidation} = require('./validations');
+
+const TOKEN = (process.env.TOKEN_SECRET || "fdgdfgdfgdfglkñjewlklekjwrljlkjsadlfkniu023984093840293lkhjkldf");
 
 var BASE_API_PATH = "/api/v1";
 
@@ -8,11 +15,13 @@ var app = express();
 
 app.use(bodyParser.json());
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
     res.send("<html><body><h1>My clients server</h1></body></html>");
 });
 
-app.get(BASE_API_PATH + "/clients", (req, res) => {
+app.get(process.env.VERSION + "/clients", authValidation, (req, res) => {
     console.log(Date() + " - GET /clients");
     Client.find({}, (err,clients)=> {
         if(err){
@@ -27,20 +36,7 @@ app.get(BASE_API_PATH + "/clients", (req, res) => {
     });
 });
 
-app.post(BASE_API_PATH + "/clients", (req, res) => {
-    console.log(Date() + " - POST /clients");
-    var client = req.body;
-    Client.create(client, (err)=>{
-        if(err){
-            console.log(Date()+ " - "+ err);
-            res.sendStatus(500);
-        }else{
-            res.sendStatus(201);
-        }
-    });
-});
-
-app.put(BASE_API_PATH + "/clients/:nif",(req,res)=>{
+app.put(process.env.VERSION + "/clients/:nif",(req,res)=>{
   console.log(Date() + " - PUT /clients/" + req.params.id);
   Client.updateOne({nif: req.params.nif}, {$set:{  
     name: req.body.name,
@@ -57,7 +53,7 @@ app.put(BASE_API_PATH + "/clients/:nif",(req,res)=>{
   });
 });
 
-app.delete(BASE_API_PATH +"/clients/:nif", async (req, res) => {
+app.delete(process.env.VERSION +"/clients/:nif", async (req, res) => {
   console.log(req.params.nif);
   Client.remove({
     nif : req.params.nif
@@ -68,5 +64,57 @@ app.delete(BASE_API_PATH +"/clients/:nif", async (req, res) => {
       res.send('Success! Client has been deleted.');	
     });
   });
-    
+
+app.post(process.env.VERSION + '/register', async (req, res) => {
+    // Validating data
+
+    const client = await Client.findOne({ username : req.body.username });
+    if(client) return res.status(400).send('There is a client with the same username');
+
+    const validation = registerValidation(req.body);
+
+    if(validation.error) return res.status(400).send(validation.error.details[0].message);
+
+    // Hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    const newClient = new Client({
+        username: req.body.username, 
+        password: hashedPassword, 
+        firstName: req.body.firstName, 
+        lastName: req.body.lastName, 
+        email: req.body.email, 
+        phone: req.body.phone, 
+        address: req.body.address
+    });
+
+    newClient.save()
+    .then((savedClient) => res.json(savedClient))
+    .catch(err => res.status(400).json('Error registering: ' + err));
+    return res;
+});
+
+app.post(process.env.VERSION + '/login', async (req, res) => {
+    // Validating data
+    const validation = loginValidation(req.body);
+
+    if(validation.error) return res.status(400).send(validation.error.details[0].message);
+
+    // Check if username exists
+    const client = await Client.findOne({ username : req.body.username });
+    if(!client) return res.status(400).send('Client does not exist');
+    // Check for password
+    const validPassword = await bcrypt.compare(req.body.password, client.password);
+    if(!validPassword) return res.status(400).send('Password is wrong');
+
+    // Create and assign a token
+    const token = jwt.sign({_id: client.id}, TOKEN, {expiresIn: '1d'});
+    res.header('auth-token', token).send({
+            token: token,
+            id: client.id
+    });
+    return res;
+});
+
 module.exports=app;
