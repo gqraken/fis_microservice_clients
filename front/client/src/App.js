5a5d6f524d4443737f663e37a23b0c99802ed991
NicolasCLEMENT1 <nicolas.clement@insa-rouen.fr>
2021-01-23 18:57:47 +0100

    Added the beginning of the functions

--- front/client/src/App.js
+++ front/client/src/App.js
@@ -8,7 +8,23 @@ class App extends React.Component {
     super()
 
   }
-  
+  //leer los clientes
+  selectClient = (client) => {
+
+  }
+  //anadir un cliente
+  createClient = (client) => {
+
+  }
+  //modificar un cliente
+  editClient = (client) => {
+
+  }
+  //eliminar un cliente
+  removeClient = (client) => {
+    
+  }
+  return ()
 }
 
 export default App;
