cf72d15d35379a860a3fcbdbe20776eacfb9ce76
gqraken <61391908+gqraken@users.noreply.github.com>
2021-01-25 00:14:34 +0100

    Update server.js
    rollback

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-const cors = require('cors');
 const Client = require('./clients');
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
@@ -13,12 +12,6 @@ const TOKEN = (process.env.TOKEN_SECRET || "fdgdfgdfgdfglkñjewlklekjwrljlkjsadl
 var BASE_API_PATH = "/api/v1";
 
 var app = express();
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 app.use(bodyParser.json());
 
