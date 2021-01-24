2f2fa8967b4a06a48057c86aede966bcaeb895e6
gqraken <61391908+gqraken@users.noreply.github.com>
2021-01-25 00:10:20 +0100

    Update server.js
    CORS support v2

--- server.js
+++ server.js
@@ -14,6 +14,12 @@ var BASE_API_PATH = "/api/v1";
 
 var app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(bodyParser.json());
 
 app.use(express.json());
