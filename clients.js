12784f912ba7f1663f33515cce8c786c1303cb3b
gqraken <61391908+gqraken@users.noreply.github.com>
2021-02-12 14:15:24 +0100

    Update clients.js

--- clients.js
+++ clients.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
 const clientSchema = new mongoose.Schema({
-    username: { type: String, required: true, trim: true, minlength: 3, maxlength: 32 },
-    password: { type: String, required: true, minlength: 6, maxlengt: 512 },
-    firstName: { type: String, required: true, trim: true, maxlength: 128 },
-    lastName: { type: String, required: true, trim: true, maxlength: 128 },
-    address: { type: String, required: true, trim: true, maxlength: 255 },
-    email: { type: String, required: true, trim: true, minlength: 6, maxlength: 255 },
-    phone: { type: String, required: true, trim: true, minlength: 9 },
+    username: { type: String, unique:false, required: true, trim: true, minlength: 3, maxlength: 32 },
+    password: { type: String, unique:false, required: true, minlength: 6, maxlengt: 512 },
+    firstName: { type: String, unique:false, required: true, trim: true, maxlength: 128 },
+    lastName: { type: String, unique:false, required: true, trim: true, maxlength: 128 },
+    address: { type: String, unique:false, required: true, trim: true, maxlength: 255 },
+    email: { type: String, unique:false, required: true, trim: true, minlength: 6, maxlength: 255 },
+    phone: { type: String, unique:false, required: true, trim: true, minlength: 9 },
 }, { 
     timestamps: true,
 }
